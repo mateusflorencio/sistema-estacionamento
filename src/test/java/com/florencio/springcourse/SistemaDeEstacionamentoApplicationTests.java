1e0d892a6473a6582e10b409a12acbb7a082899b
Mateus Florêncio <mateuusflorencio@outlook.com.br>
2022-03-03 16:18:44 -0300

    first commit

---
+++ src/test/java/com/florencio/springcourse/SistemaDeEstacionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florencio.springcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeEstacionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
